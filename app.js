87d0f21b2a31e4d39a8dc4c0127ad5bcf4ce5c0d
Bruce Zhu <brucezzhu@gmail.com>
2022-01-21 16:43:27 +1100

    set up post req to create new campground

--- app.js
+++ app.js
@@ -19,24 +19,39 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'My Backyard',
-        description: "cheap camping!"
-    });
-    await camp.save();
-    
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+// create a campground
+app.post('/campgrounds', async (req, res) => {
+    // ceate a new campground model for every new campground
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// order matters, this one cannot be above "campgrounds/new"
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
 })
 
 
 
+
 app.listen(port, () => {
-    console.log(`Listen on port ${port}!!!`)
+    console.log(`Listen on http://localhost:${port}`)
 })
