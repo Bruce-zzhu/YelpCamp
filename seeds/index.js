eddca458e789c81006642aac6a12017d2e939101
Bruce Zhu <brucezzhu@gmail.com>
2022-01-23 17:51:17 +1100

    get image api

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Campground = require('../models/campground');
 const cities = require('./cities')  // 1000 cities
 const {places, descriptors} = require('./seedHelpers')
+const axios = require("axios");
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -19,9 +20,22 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);   // a random city (out of 1000 cities)
+        const randPrice = Math.floor(Math.random() + 20) + 10;
+        // https://api.unsplash.com/photos/random?collections=483251&client_id=05T3GxU3h_nKIVb5iXLnuxP9CDvGDcEgk_i4qjpXJDo
+        const image = await axios.get('https://api.pexels.com/v1/photos/2422265')
+                            .then(res => {
+                                return res.data.src.large
+                            })
+                            .catch(err => {
+                                console.log("Fetch image error", err)
+                            })
+
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: image,
+            description: 'lalalalala',
+            price: randPrice
         })
         await camp.save()
     }
