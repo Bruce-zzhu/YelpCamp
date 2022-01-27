a39a65afd993e4692c2a54d0d9478948dacb6dfa
Bruce Zhu <brucezzhu@gmail.com>
2022-01-27 11:26:38 +1100

    add basic data validation

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require('./models/campground');
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 const Review = require('./models/review');
+
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -26,15 +30,37 @@ app.use(methodOverride('_method'));  // '_method' can be editted, but in the for
 app.engine('ejs', ejsMate)  // use the ejs-mate engine
 
 
+// validate data with joi
+const validateCampground = (req, res, next) => {
+    // define joi schema
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body)
+    if(error) {
+        // map over the element of each obj and return message
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 // list of campgrounds
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 // campground creation page
 app.get('/campgrounds/new', (req, res) => {
@@ -42,48 +68,60 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 // create a campground via a form
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // ceate a new campground model for every new campground
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // order matters, this one cannot be above "campgrounds/new"
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
-})
+}))
 
 // submit review
-app.post('/campgrounds/:id/reviews', async (req, res) => {
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
     
-})
+}))
 
 // campground edit page
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground })
-})
+}))
 
 // update campground
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // delete campground
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+// for every request and every path, pass error to error handler
+app.all('*', (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
 })
 
-
+// Error handler
+app.use((err, req, res, next) => {
+    
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = "Oh No, Something Went Wrong!";
+    res.status(statusCode).render('error', { err });
+    
+})
 
 
 
 app.listen(port, () => {
-    console.log(`Listen on http://localhost:${port}`)
+    console.log(`Listen on http://localhost:${port}/campgrounds`)
 })
