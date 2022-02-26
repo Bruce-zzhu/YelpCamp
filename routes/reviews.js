f9be3b3c6f91dd6805fea0603682a4ee3f1e594b
Bruce Zhu <brucezzhu@gmail.com>
2022-02-27 09:43:24 +1100

    clean code by controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,28 +5,12 @@ const Review = require('../models/review');
 const Campground = require('../models/campground');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
-
+const reviews = require('../controllers/reviews')
 
 
 // submit review
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params
-    await Review.findByIdAndDelete(reviewId);
-    // delete the reference in campground.reviews
-    await Campground.findByIdAndUpdate(id,  {$pull: {reviews: reviewId}})
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
